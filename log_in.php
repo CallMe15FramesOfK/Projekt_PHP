b06cebc5a89c0b1b27c646639c19d730c037b3f7
CallMe15FramesOfK <janekkrzeczo@gmail.com>
2023-12-01 15:02:41 +0100

    Changes made to log_in and signup

--- log_in.php
+++ log_in.php
@@ -14,6 +14,7 @@
             <button type="submit">Log in</button>
             <div class="signin-link">
                 <p>Don't have an account? <a href="./signup.php">Sign in</a></p>
+                <p><a href="./index.php">Go back to homepage</a></p>
             </div>
         </form>
     </div>
--- signup.php
+++ signup.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign up</title>
+    <link rel="stylesheet" href="./styles/style_login.css">
 </head>
 <body>
-    
+    <div class="container">
+        <form class="login-form">
+            <input type="text" placeholder="Username" required>
+            <input type="password" placeholder="Password" required>
+            <input type="email" placeholder="Email" required>
+            <button type="submit">Sign up</button>
+            <div class="signin-link">
+                <p>Already have an account? <a href="./log_in.php">Log in</a></p>
+                <p><a href="./index.php">Go back to homepage</a></p>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
