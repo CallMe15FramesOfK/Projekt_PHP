4342d3a173b9f2389585df8c5be96d88bfcc750c
K1ELON <kielian.mi@gmail.com>
2023-12-01 16:32:10 +0100

    zmiany w contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,51 @@
+<?php
+
+// Replace with your actual database credentials
+$db_host = 'localhost';
+$db_name = 'gamingshop';
+$db_user = 'root';
+$db_pass = '';
+
+try {
+    // Create a new PDO connection
+    $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Check if the form has been submitted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Get the submitted data
+        $name = $_SESSION['user'];
+        $email = trim($_POST['email']);
+        $message = trim($_POST['message']);
+
+        // Check if any of the input fields are empty
+        if (empty($email) || empty($message)) {
+            echo "<p>Proszę wypełnić wszystkie pola formularza.</p>";
+        } else {
+            // Prepare the SQL query
+            $sql = "INSERT INTO comments (name, email, message) VALUES (:name, :email, :message)";
+            $stmt = $conn->prepare($sql);
+
+            // Bind the submitted data to the prepared statement
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':message', $message);
+
+            // Execute the prepared statement
+            $stmt->execute();
+
+            // Redirect the user to the comments page
+            header('Location: comments.php');
+            exit;
+        }
+    }
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +63,7 @@
                 <li><a href="./games.php">Gry</a></li>
                 <li><a href="./console.php">Konsole</a></li>
                 <li><a href="./contact.php">Kontakt</a></li>
-                <li><a href="./log_in.php">Logowanie</a></li>
+                <li><a href="./log_in.php">Zaloguj</a></li>
             </ul>
         </nav>
     </header>
@@ -30,9 +78,6 @@
 
         <h2>Lub wyślij do nas wiadomość:</h2>
         <form action="send_mail.php" method="post">
-            <label for="name">Imię:</label>
-            <input type="text" id="name" name="name" required>
-            <br>
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required>
             <br>
