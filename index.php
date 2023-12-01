bbb136880734242fe2390420a125c2fe4da92cdd
CallMe15FramesOfK <janekkrzeczo@gmail.com>
2023-12-01 13:55:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+    </header>
+    <div class="container">
+        <h1>Welcome to Our Website</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit delectus itaque doloribus, ad deleniti tempore alias odio, iusto nam deserunt, consequuntur et assumenda cum eligendi ex hic reprehenderit dicta. Tempora.</p>
+    </div>
+    <footer>
+        <p>&copy; 2022 My Website. All rights reserved.</p>
+    </footer>
+</body>
+</html>
