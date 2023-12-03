10f6ae6a4cd764e78991a31b9892c4af331b25a7
K1ELON <kielian.mi@gmail.com>
2023-12-03 13:14:44 +0100

    poprawy panelu admina

--- admin_panel.php
+++ admin_panel.php
@@ -6,24 +6,59 @@ if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     exit;
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel</title>
+    <title>Panel admina</title>
     <link rel="stylesheet" href="./styles/style_main.css">
 </head>
+
 <body>
-    <h1>Welcome, <?php echo $_SESSION['user']['username']; ?>!</h1>
-    <nav>
-        <ul>
-            <li><a href="index.php">Dashboard</a></li>
-            <li><a href="manage_users.php">Manage Users</a></li>
-            <li><a href="settings.php">Settings</a></li>
-            <li><a href="logout.php">Logout</a></li>
-        </ul>
-    </nav>
+    <header>
+        <h1>Witaj, <?php echo $_SESSION['user']['username']; ?>!</h1>
+        <nav>
+            <ul>
+                <li><a href="./index.php">Strona Główna</a></li>
+                <li><a href="./games.php">Gry</a></li>
+                <?php if (!isset($_SESSION['logged_in'])) : ?>
+                <li><a href="./admin_panel.php">Panel admina</a></li>   
+                <?php endif; ?>
+                <?php if (!isset($_SESSION['logged_in'])) : ?>
+                <li><a href="./log_in.php">Logowanie</a></li>
+                <?php endif; ?>
+                <?php if (isset($_SESSION['logged_in'])) : ?>
+                    <li><a href="./log_out.php">Wylogowanie</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="admin-section">
+            <h2>Panel admina:</h2>
+            <form action="./PHP_connections/add_game.php" method="post">
+                <label for="game_name">Nazwa gry:</label>
+                <input type="text" id="game_name" name="game_name" required>
+                <br>
+                <label for="game_price">Cena gry:</label>
+                <input type="number" id="game_price" name="game_price" min="0" step="0.01" required>
+                <br>
+                <input type="submit" value="Dodaj gre">
+            </form>
+            <br>
+            <form action="./PHP_connections/delete_game.php" method="post">
+                <label for="game_name">Nazwa gry do usunięcia:</label>
+                <input type="text" id="game_name" name="game_name" required>
+                <br>
+                <input type="submit" value="Usuń gre">
+            </form>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 Sklep dla graczy. <a href="./contact.php">Kontakt</a></p>
+    </footer>
 </body>
+
 </html>
